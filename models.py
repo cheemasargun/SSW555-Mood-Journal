6767129510de513d43828f8754f3b99756386f29
cheemasargun <146683276+cheemasargun@users.noreply.github.com>
2025-11-19 20:17:25 -0500

    Updated database

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 from extensions import db
-from datetime import datetime
+from datetime import datetime, date
 import json
 
 class MoodEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     mood = db.Column(db.String(50), nullable=False)
-    notes = db.Column(db.Text)
+    note = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     # 🔹 NEW FIELDS for mood journal integration
