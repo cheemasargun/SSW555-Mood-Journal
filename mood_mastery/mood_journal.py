b1326ffa5af7a8a9624f964e7da9d9240897b1cc
cheemasargun <146683276+cheemasargun@users.noreply.github.com>
2025-11-20 21:09:02 -0500

    Update mood_journal.py

--- mood_mastery/mood_journal.py
+++ mood_mastery/mood_journal.py
@@ -1,32 +1,5 @@
-"""
-General thoughts: this is where we'd have the mood journal
-
-mood_journal.py would likely refer to some kind of database (see docs/DATABASE.md) and get any pre-existing entries
-
-These entries would be loaded into the appropriate object/variable, and here we would have the functions to add/create/delete
-(esp since we can't exactly delete an object/instance of a class *from* that object/instance)
-(so the delete method would definitely have to go here/wherever it is we're gathering and modifying entries)
-
-REFERENCES TO LOOK AT: docs/DATABASE.md, 
-"""
-
-"""
-Example from DATABASE.md:
-
-class MoodEntry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mood = db.Column(db.String(20), nullable=False)
-    note = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-"""
-
-"""
-^ This example seems to be doing what we're doing in entry.py
-We can make further edits to mood_journal.py and entry.py in accordance to the examples given if we
-wish to follow them to a T, or we can see how we'd do the equivalents with what we currently have
-in entry.py and mood_journal.py
-"""
-
+from flask import current_app
+from models import MoodEntry
 from extensions import db
 from datetime import datetime, date, timedelta
 from mood_mastery.entry import Entry
@@ -55,9 +28,6 @@ class Mood_Journal:
         self.last_entry_date = None
 
         try:
-            from flask import current_app
-            from models import MoodEntry
-            
             with current_app.app_context():
                 rows = MoodEntry.query.order_by(MoodEntry.entry_date.asc(), MoodEntry.created_at.asc()).all()
                 for row in rows:
